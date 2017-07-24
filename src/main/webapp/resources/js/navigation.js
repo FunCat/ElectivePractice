faca9ee190309012482dfe7663bd15319eb7603e
Rustam <dr.rustam@gmail.com>
2017-07-24 12:06:54 +0300

    new branch with bootstrap, navigation and structure changes

--- src/main/webapp/resources/js/navigation.js
+++ src/main/webapp/resources/js/navigation.js
@@ -19,12 +19,18 @@ $("#prevPage").click(function () {
     };
 
     $.ajax({
+        contentType:'application/json',
         dataType: 'json',
         type: 'POST',
-        data:a,
+        data: JSON.stringify(a),
         url: contextPath + "/news.all",
         success: function (response) {
             console.log(response);
+            // $("#coursesList").html("");
+            $.each(response.data, function( index, value ) {
+                console.log(index, value);
+                $("#coursesList").append("<li>" + value.name + "</li>")
+            });
         }
     });
 
